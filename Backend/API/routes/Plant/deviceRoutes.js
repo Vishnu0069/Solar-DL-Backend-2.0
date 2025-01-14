624dfa9f7e33933a26afc0905b91477a71866a7a
MohanaAntsAI <mohana@antsai.in>
2025-01-14 22:54:41 +0530

    fixe the error in device

--- Backend/API/routes/Plant/deviceRoutes.js
+++ Backend/API/routes/Plant/deviceRoutes.js
@@ -15,7 +15,7 @@ router.post(
     body("Plant_id").notEmpty().withMessage("Plant_id is required"),
     body("user_id").notEmpty().withMessage("user_id is required"),
     body("Device_type").notEmpty().withMessage("Device_type is required"),
-    body("model").notEmpty().withMessage("Model is required"), // Ensure model validation
+    //body("model").notEmpty().withMessage("Model is required"), // Ensure model validation
   ],
   async (req, res) => {
     const errors = validationResult(req);
